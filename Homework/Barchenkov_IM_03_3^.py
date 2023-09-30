39adc1615d43d81c08afaba039626e5632223b68
nevadiesel <ivan@neva-diesel.com>
2023-09-30 11:37:30 +0300

    edit Homework_3

--- Homework/Barchenkov_IM_03_3^.py
+++ Homework/Barchenkov_IM_03_3^.py
@@ -1,20 +1,33 @@
-#Вывод алфавита
+# Вывод алфавита
 result = []
 for c in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
-    result.append("|"+c.upper()+c+"|")
+    result.append("|" + c.upper() + c + "|")
 print(20*'-')
 for i in range(0, len(result), 5):
     print(''.join(result[i:i+5]), "\n" + "-"*20)
 
 
+# Рамочка
+m, n, nn = int(input("Горизонталь: ")), int(input("Вертикаль: ")), 0
+space = (m - 2) * " "
+print(m*"@")
+while nn < n - 2:
+    print(f"@{space}@")
+    nn += 1
+print(m*"@") if n != 1 else 0
+
+
 # Задание №1
 lim = int(input("Верхний предел: "))
-summa = 0
-num = 1
-while num <= lim:
-    summa += num ** 3
-    num += 1
-print(summa)
+if lim <= 100:
+    summa = 0
+    num = 1
+    while num <= lim:
+        summa += num ** 3
+        num += 1
+    print(summa)
+else:
+    print("Ошибка! Вводи число не больше 100.")
 
 
 # Задание №2
