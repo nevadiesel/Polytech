19f1252f06ff4569f0d866b08b361b5c76119ac2
IvanBarchenkov <ivan@neva-diesel.com>
2024-03-14 20:30:51 +0300

    Lesson_6

--- Lessons term 2/Lesson_6.py
+++ Lessons term 2/Lesson_6.py
@@ -8,29 +8,28 @@ class Derivate:
     def __init__(self, func):
         self.func = func
 
-    def __call__(self, a, x, dx=0.0001):
-        return (self.func(a, x + dx) - self.func(a, x)) / dx
+    def __call__(self, x, a, dx=0.0001):
+        return (self.func(x + dx, a) - self.func(x, a)) / dx
 
 
 class Plot:
     def __init__(self, func):
         self.func = func
 
-    def __call__(self, a, x):
+    def __call__(self, x, a):
         x = np.linspace(-1, 3, 100)
-        y = self.func(a, x)
+        y = self.func(x, a)
         plt.plot(x, y)
         plt.grid(True)
         plt.show()
 
 
-# @Plot
-@Derivate
 @Plot
-def func(a, x):
-    return math.e ** (a ** x)
+@Derivate
+def func(x, a):
+    return math.e ** (x ** a)
 
 def sin(x):
     return math.sin(x)
 
-print(func(1, 3))
+func(1, 2)
