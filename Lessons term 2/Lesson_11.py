b09cb58e4a031ff0bc2e693ff785b4554bf78c9c
IvanBarchenkov <60175306+nevadiesel@users.noreply.github.com>
2024-04-24 22:41:57 +0300

    11

--- Lessons term 2/Lesson_11.py
+++ Lessons term 2/Lesson_11.py
@@ -4,22 +4,25 @@ class Person:
         self.phone = phone
         self.__salary = salary
 
-    def set(self, salary):
-        self.__salary = salary
-
     def get(self):
         return int(self.__salary)
+
+    def set(self, salary):
+        if isinstance(self, Managers):
+            self.__salary = salary * 1.5
+        elif isinstance(self, Developers):
+            self.__salary = salary * 1
+        else:
+            self.__salary = salary
 
 
 class Managers(Person):
     def __init__(self, name, phone, salary=0):
         super().__init__(name, phone, salary)
 
-
 class Developers(Person):
     def __init__(self, name, phone, salary=0):
         super().__init__(name, phone, salary)
-
 
 x = Managers('Stas', '+7 911 1234567')
 x.set(1000)
@@ -30,38 +33,40 @@ y.set(1000)
 print(y.get())
 
 
-
-class Person:
+'''class Person:
     def __init__(self, name, phone, salary=0):
         self.name = name
         self.phone = phone
         self.__salary = salary
 
-    def set(self, salary):
-        # Определение класса объекта и установка salary с коэффициентом
-        if isinstance(self, Managers):
-            self.__salary = salary * 1.5
-        elif isinstance(self, Developers):
-            self.__salary = salary * 1.2
-        else:
-            self.__salary = salary
-
     def get(self):
-        return int(self.__salary)
+        return self.__salary
 
-class Managers(Person):
-    def __init__(self, name, phone, salary=0):
-        super().__init__(name, phone, salary)
+    def set_salary(self, new_salary):
+        self.__salary = new_salary
 
-class Developers(Person):
-    def __init__(self, name, phone, salary=0):
-        super().__init__(name, phone, salary)
 
-# Тестирование классов с различными коэффициентами для salary
-x = Managers('Stas', '+7 911 1234567')
-x.set(1000)
-print(x.get())  # Вывод: 1500
+class Manager(Person):
+    def __init__(self, name, phone):
+        super().__init__(name, phone)
 
-y = Developers('Ivan', '+7 911 9876543')
-y.set(1000)
-print(y.get())  # Вывод: 1200
+    def set_salary(self, new_salary):
+        super().set_salary(new_salary)
+
+
+class Developer(Person):
+    def __init__(self, name, phone):
+        super().__init__(name, phone)
+
+    def set_salary(self, new_salary):
+        super().set_salary(int(new_salary * 1.2))
+
+
+m = Manager("Bob", "654321")
+d = Developer("Charlie", "987654")
+m.set_salary(1000)
+d.set_salary(1000)
+
+print(m.get())
+print(d.get())
+'''
