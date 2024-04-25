c36c1bd008c30e38e5df7c7c438c9aa98e35fe86
IvanBarchenkov <60175306+nevadiesel@users.noreply.github.com>
2024-04-25 18:59:45 +0300

    upd

--- Lessons term 2/Lesson_12.py
+++ Lessons term 2/Lesson_12.py
@@ -5,10 +5,10 @@ class Person:
         self.phone = phone
         self.__salary = salary
 
-    def get(self):
+    def get_salary(self):
         return int(self.__salary)
 
-    def set(self, salary):
+    def set_salary(self, salary):
         if isinstance(self, Manager):
             self.__salary = salary * 1.5
         elif isinstance(self, Developer):
@@ -16,36 +16,41 @@ class Person:
         else:
             self.__salary = salary
 
-    def toggle_vacation(self, destination=''):
-        self.on_vacation = not self.on_vacation
-        return f'{type(self).__name__} {self.name} is now {"on vacation" if self.on_vacation else "back to work"} {destination}.'
+    def on_vacation(self, destination=None):
+        self.vacation = True
+        print(f'{type(self).__name__} {self.name} is now on vacation in {destination}.')
+
+    def from_vacation(self):
+        self.vacation = False
+        print(f'{type(self).__name__} {self.name} back to work.')
 
 
 class Preson_id:
-    Preson_id = 2
+    ID = 2
 
     def __init__(self):
-        Preson_id.Preson_id += 1
-        self.Preson_id = Preson_id.Preson_id
+        Preson_id.ID += 1
+        self.ID = Preson_id.ID
 
 
 class Manager(Person, Preson_id):
-    def __init__(self, name, phone, salary=0, on_vacation=False):
+    def __init__(self, name, phone, salary=0):
         super().__init__(name, phone, salary)
-        self.on_vacation = on_vacation
 
 
 class Developer(Person, Preson_id):
-    def __init__(self, name, phone, salary=0, on_vacation=False):
+    def __init__(self, name, phone, salary=0):
         super().__init__(name, phone, salary)
-        self.on_vacation = on_vacation
 
 
 m = Manager("Bob", "654321")
 d = Developer("Richard", "987654")
-m.set(1000)
-d.set(1000)
+m.set_salary(1000)
+d.set_salary(1000)
 
-print(m.toggle_vacation())
-print(m.__dict__, m.get())
-print(d.__dict__, d.get())
+print(m.__dict__, m.get_salary())
+m.on_vacation()
+print(m.__dict__, m.get_salary())
+print(d.__dict__, d.get_salary())
+m.from_vacation()
+print(m.__dict__, m.get_salary())
