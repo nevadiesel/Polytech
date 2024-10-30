762020f78d7cf062260e33c6f143157dc22ffb6e
IvanBarchenkov <60175306+nevadiesel@users.noreply.github.com>
2024-10-30 10:33:03 +0300

    1

--- Lessons term 2/Lesson_12.py
+++ Lessons term 2/Lesson_12.py
@@ -1,16 +1,16 @@
 class Person:
     def __init__(self, name, phone, salary=0):
-        super().__init__()
         self.name = name
         self.phone = phone
         self.__salary = salary
+        self.is_active = True
 
     def get_salary(self):
         return int(self.__salary)
 
     def set_salary(self, salary):
         if isinstance(self, Manager):
-            self.__salary = salary * 1.5
+            self.__salary = salary * 1.2
         elif isinstance(self, Developer):
             self.__salary = salary * 1
         else:
@@ -24,33 +24,43 @@ class Person:
         self.vacation = False
         print(f'{type(self).__name__} {self.name} back to work.')
 
+    @staticmethod
+    def hire(name, vacancy, phone):
+        if vacancy == 'manager':
+            print(f'Manager {name} has been hired.')
+            return Manager(name, phone)
+        else:
+            print(f'Developer {name} has been hired.')
+            return Developer(name, phone)
 
-class Preson_id:
+    def fire(self):
+        self.is_active = False
+        print(f'{type(self).__name__} {self.name} has been terminated.')
+
+
+class Person_id:
     ID = 2
 
     def __init__(self):
-        Preson_id.ID += 1
-        self.ID = Preson_id.ID
+        Person_id.ID += 1
+        self.ID = Person_id.ID
 
 
-class Manager(Person, Preson_id):
+class Manager(Person, Person_id):
     def __init__(self, name, phone, salary=0):
         super().__init__(name, phone, salary)
 
 
-class Developer(Person, Preson_id):
+class Developer(Person, Person_id):
     def __init__(self, name, phone, salary=0):
         super().__init__(name, phone, salary)
 
 
-m = Manager("Bob", "654321")
-d = Developer("Richard", "987654")
-m.set_salary(1000)
-d.set_salary(1000)
-
-print(m.__dict__, m.get_salary())
-m.on_vacation()
-print(m.__dict__, m.get_salary())
-print(d.__dict__, d.get_salary())
-m.from_vacation()
-print(m.__dict__, m.get_salary())
+alice = Person.hire('Alice', 'manager', '654321')
+alice.set_salary(1000)
+print(alice.__dict__, alice.get_salary())
+alice.on_vacation("Hawaii")
+print(alice.__dict__, alice.get_salary())
+alice.from_vacation()
+alice.fire()
+print(alice.__dict__, alice.get_salary())
